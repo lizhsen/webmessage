c82c41a181624f9c67a6a1c9da90122b8675378b
lizhsen <lizhsen@163.com>
2018-01-30 20:28:17 +0800

    pratise

---
+++ message/views.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from .models import UserMessage
+
+# Create your views here.
+
+
+def getform(request):
+    if request.method == "POST":
+        name = request.POST.get('name', '')
+        message = request.POST.get('message', '')
+        address = request.POST.get('address', '')
+        email = request.POST.get('email', '')
+        user_message = UserMessage()
+        user_message.name = name
+        user_message.message = message
+        user_message.email = email
+        user_message.address = address
+
+        user_message.save()
+
+    return render(request, 'message_form.html')
